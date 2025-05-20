8dcb98c8833c140ba243f269be46a0c2c09aeeac
mattmetzgar <55600169+mattmetzgar@users.noreply.github.com>
2025-05-20 11:59:38 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Overnight AI Educator";
+export const SITE_DESCRIPTION = "Overnight AI Educator: Transform Your Course in 24 Hours";
