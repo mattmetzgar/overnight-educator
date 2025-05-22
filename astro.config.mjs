e5c2ec1a45fef447f9ba0f0a5353eb49e8940860
elishaadamu <elishadamu97@gmail.com>
2025-05-22 17:52:06 +0100

    resolve error

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,14 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://overnight.education",
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  // Remove or comment out the adapter line below if not using SSR:
+  // adapter: cloudflare({
+  //   platformProxy: {
+  //     enabled: true,
+  //   },
+  // }),
 });
